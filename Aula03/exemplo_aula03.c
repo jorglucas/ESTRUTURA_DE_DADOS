fb88c2a90ac85733119f03f0a0b666c288d5edd5
Jhn <jhonata.tenorio2526@gmail.com>
2021-02-28 20:09:44 -0300

    exemplo_aula03.c

---
+++ Aula03/exemplo_aula03.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define MAX 50
+
+typedef int TIPOCHAVE;
+
+typedef struct {
+    TIPOCHAVE chave;
+} REGISTRO;
+
+typedef struct {
+    REGISTRO A[MAX];
+    int numeroDeElementos;
+} LISTA;
+
+int main(){
+    int definirTamanho();
+    void exibirLista();
+    LISTA l;
+    l.numeroDeElementos = definirTamanho(&l);
+    for(int i = 0; i < l.numeroDeElementos; i++){
+        l.A[i].chave = i + 20;
+    }
+    exibirLista(&l);
+}
+
+int definirTamanho(LISTA* l){
+    l->numeroDeElementos = 10;
+    return l->numeroDeElementos;
+}
+
+void exibirLista(LISTA* l){
+    printf("Lista: \" ");
+    for(int i = 0; i < l->numeroDeElementos; i++){
+        printf("%d ", l->A[i].chave);
+    }
+
+    printf("\"\n");
+}
