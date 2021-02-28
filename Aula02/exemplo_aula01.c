a262a54248d2a7dc779cc78e74f3b4117ed6dbe6
Jhn <jhonata.tenorio2526@gmail.com>
2021-02-28 18:12:32 -0300

    retirando ; do #define

--- Aula02/exemplo_aula01.c
+++ Aula02/exemplo_aula01.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#define alturaMaxima 225; // O compilador define a palavra 'alturaMAxima' como sendo 225.
+#define alturaMaxima 225 // O compilador define a palavra 'alturaMAxima' como sendo 225.
 
 /*
     A sintaxe struct{...} define uma estrutura com os campos definidos dentro das chaves;
