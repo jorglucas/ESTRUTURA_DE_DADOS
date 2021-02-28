2ee68c342386fdf5f5e22cee6a5c2d1924fae550
Jhn <jhonata.tenorio2526@gmail.com>
2021-02-28 17:31:14 -0300

    exemplo_aula03.c

---
+++ Aula02/exemplo_aula03.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int* y = (int*) malloc(sizeof(int)); // Aloca a quantidade de memória de um inteiro( 32bits => 4 bytes / 64bits => 8bytes);
+    *y = 20; // Atribuindo o valor 20 para o endereço alocado para y;
+    int z = sizeof(int); // retorna o tamanho de um inteiro; 
+    printf("*y = %d\nz = %d\n", *y, z);
+}
